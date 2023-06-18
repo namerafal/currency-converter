374d9523f89dd8e5c77caef606d3155944fcf11b
Rafał Olewicz <namerafal75@gmail.com>
2023-06-18 16:59:08 +0200

    Format document

--- js/script.js
+++ js/script.js
@@ -5,22 +5,22 @@
         const gbpRate = 5.2874;
 
         switch (currency) {
-          case "EUR":
-            return amount * euroRate;
+            case "EUR":
+                return amount * euroRate;
 
-          case "USD":
-            return amount * usdRate;
+            case "USD":
+                return amount * usdRate;
 
-          case "GBP":
-            return amount * gbpRate;
+            case "GBP":
+                return amount * gbpRate;
         }
-      };
+    };
 
-      const updateResultText = (resultElement, amount, currencyElement, result) => {
+    const updateResultText = (resultElement, amount, currencyElement, result) => {
         resultElement.innerHTML = `<strong> ${amount.toFixed(2)} ${currencyElement.value} = ${result.toFixed(2)} PLN </strong>`;
-      };
+    };
 
-      const onFormSubmit = (event, resultElement) => {
+    const onFormSubmit = (event, resultElement) => {
         event.preventDefault();
         const amountElement = document.querySelector(".js-amount");
         const currencyElement = document.querySelector(".js-currency");
@@ -28,20 +28,20 @@
         const result = calculateResult(amount, currencyElement.value);
 
         updateResultText(resultElement, amount, currencyElement, result);
-      };
+    };
 
-      const clearForm = (resultElement) => {
+    const clearForm = (resultElement) => {
         resultElement.innerText = "--Wynik--";
-      };
+    };
 
-      const init = () => {
+    const init = () => {
         const formElement = document.querySelector(".js-form");
         const resultElement = document.querySelector(".js-result");
 
         formElement.addEventListener("submit", (event) => onFormSubmit(event, resultElement));
         formElement.addEventListener("reset", () => clearForm(resultElement));
-      };
+    };
 
-      init();
+    init();
 
 } 
